c0fae5ac425c061d6d1dd03bdd9e3aff2cf2cb11
void-alive <sja9284@gmail.com>
2025-01-24 09:07:34 +0900

    chap4

---
+++ JavaWeb/CHAP4/src/main/java/com/example/chap4/Chap4Application.java
@@ -0,0 +1,13 @@
+package com.example.chap4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap4Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Chap4Application.class, args);
+  }
+
+}
---
+++ JavaWeb/CHAP4/src/test/java/com/example/chap4/Chap4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chap4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap4ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
